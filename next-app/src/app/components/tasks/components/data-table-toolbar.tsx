6f9c41aec6b8593310e1dc501cee0a8cdff2c1cc
aakash2330 <aakash2330@gmail.com>
2024-10-23 01:09:30 +0530

    api-spec

--- next-app/src/app/components/tasks/components/data-table-toolbar.tsx
+++ next-app/src/app/components/tasks/components/data-table-toolbar.tsx
@@ -107,7 +107,7 @@ export function DataTableToolbar<TData>({
         <DialogTrigger
           className={cn(
             "inline-flex h-8 rounded-md px-3 text-xs items-center justify-center gap-2 whitespace-nowrap font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
-            "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
+            "border border-input bg-black shadow-sm text-white hover:text-black hover:bg-background hover:text-accent-foreground",
           )}
         >
           Send Submission
--- next-app/src/app/page.tsx
+++ next-app/src/app/page.tsx
@@ -1,10 +1,14 @@
+import SwaggerUIComponent from "./components/swagger/main";
 import TaskPage from "./components/tasks/main";
+
+export const dynamic = "force-dynamic";
 
 export default function Home() {
   return (
     <main className="flex flex-col gap-8 w-full justify-center items-center ">
       <div className="w-[60rem]">
         <TaskPage></TaskPage>
+        <SwaggerUIComponent></SwaggerUIComponent>
       </div>
     </main>
   );
