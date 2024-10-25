51101dbe3e2274a1134e9e07b79779a4b0d4f1b2
aakash2330 <aakash2330@gmail.com>
2024-10-26 01:55:45 +0530

    challenges/ route protect

--- next-app/src/middleware.ts
+++ next-app/src/middleware.ts
@@ -6,6 +6,10 @@ export async function middleware(req: NextRequest) {
   const token = await getToken({ req, secret: process.env.NEXT_AUTH_SECRET });
   const { pathname } = new URL(req.url);
 
+  if (!token && pathname.startsWith("/challenges/")) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
   if (!token && pathname === "/admin/challenge/create") {
     return NextResponse.redirect(new URL("/login", req.url));
   }
